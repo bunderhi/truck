1c30b73f3f5ccefe41e3c768e71b8e115b5b79c4
bunderhi <bunderhi@gmail.com>
2020-09-20 14:37:40 -0600

    add services

---
+++ services/installservice.sh
@@ -0,0 +1,8 @@
+sudo cp /home/projects/truck/services/startcar.service /etc/systemd/system/startcar.service
+#
+#Add the rule (file 99-local.rules) to start system unit service when ttyUSB0 is available
+sudo cp  /homels/projects/truck/services/99-startcar.rules /etc/udev/rules.d/99-local.rules
+#
+sudo udevadm control -l debug           #allows debugging (tail –f /var/log/syslog, and plug/unplug a usb stick)
+sudo udevadm control --reload-rules     #reloads rules
+sudo systemctl daemon-reload            #reloads systemd
