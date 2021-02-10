7ecbc5edfa0e943ae2a2f2c0f18dfb6225bf2b0c
Brian Underhill <bunderhi@gmail.com>
2021-02-10 11:20:09 -0700

    add text

--- tests/birdseye.py
+++ tests/birdseye.py
@@ -106,6 +106,61 @@ def warpperspective(img):
 
     return dst
 
+def draw_text(
+        img,
+        *,
+        text,
+        uv_top_left,
+        color=(255, 255, 255),
+        fontScale=0.5,
+        thickness=1,
+        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+        outline_color=(0, 0, 0),
+        line_spacing=1.5,
+    ):
+        """
+        Draws multiline with an outline.
+        """
+        assert isinstance(text, str)
+
+        uv_top_left = np.array(uv_top_left, dtype=float)
+        assert uv_top_left.shape == (2,)
+
+        for line in text.splitlines():
+            (w, h), _ = cv2.getTextSize(
+                text=line,
+                fontFace=fontFace,
+                fontScale=fontScale,
+                thickness=thickness,
+            )
+            uv_bottom_left_i = uv_top_left + [0, h]
+            org = tuple(uv_bottom_left_i.astype(int))
+
+            if outline_color is not None:
+                cv2.putText(
+                    img,
+                    text=line,
+                    org=org,
+                    fontFace=fontFace,
+                    fontScale=fontScale,
+                    color=outline_color,
+                    thickness=thickness * 3,
+                    lineType=cv2.LINE_AA,
+                )
+            cv2.putText(
+                img,
+                text=line,
+                org=org,
+                fontFace=fontFace,
+                fontScale=fontScale,
+                color=color,
+                thickness=thickness,
+                lineType=cv2.LINE_AA,
+            )
+
+            uv_top_left += [0, h * line_spacing]
+
+
 def main(img_path,out_path):
     
     alpha = 0.5
@@ -139,6 +194,16 @@ def main(img_path,out_path):
     birdseye_img = warpperspective(undistorted)
     markup_img = realpolygon(birdseye_img)
     
+    velx = 0.1
+    vely = 0.01
+    velz = 0.05
+    vx = "{:.1f}".format(velx *100)
+    vy = "{:.1f}".format(vely *100)
+    vz = "{:.1f}".format(velz *100)
+    lines = vx + '\n' + vz + '\n' + vy
+    draw_text(markup_img,text=lines,uv_top_left=(120,240))
+
+
     plt.subplot(131),plt.imshow(original),plt.title('Original')
     plt.subplot(132),plt.imshow(undistorted),plt.title('Undistorted')
     plt.subplot(133),plt.imshow(markup_img),plt.title('Birdseye')
