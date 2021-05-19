d3eeea146ac5096552b436ba7d647f83f57b4857
bunderhi <bunderhi@gmail.com>
2021-05-19 10:33:16 -0600

    add network stats

--- nextion_test.py
+++ nextion_test.py
@@ -5,6 +5,47 @@ import subprocess
 import os
 import nextion_lib as nxlib
 
+
+def get_ip_address(interface):
+    state = get_network_interface_state(interface)
+    if state == 'down' or state == None:
+        return None
+
+    cmd = "ifconfig %s | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'" % interface
+    return subprocess.check_output(cmd, shell=True).decode('ascii')[:-1]
+
+
+def get_network_interface_state(interface):
+    if not os.path.exists('/sys/class/net/%s/operstate' % interface):
+        #print("%s file does NOT exist" % interface)
+        return None
+
+    try:
+        status = subprocess.check_output('cat /sys/class/net/%s/operstate' % interface, shell=True).decode('ascii')[:-1]
+    except Exception as err:
+        print("Exception: {0}".format(err))
+        return None
+    else:
+        return status
+
+# Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
+CPU = subprocess.check_output(cmd, shell = True )
+cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
+MemUsage = subprocess.check_output(cmd, shell = True )
+cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
+Disk = subprocess.check_output(cmd, shell = True )
+
+
+netstat = get_ip_address('wlan0')
+if netstat is not None:
+    netstattxt = netstat
+else:
+    netstattxt = 'Down'
+
+freememtxt = str(MemUsage.decode('utf-8'))
+freedisktxt = str(Disk.decode('utf-8'))
+
 ######### make connection to serial UART to read/write NEXTION
 ser = nxlib.ser
 EndCom = "\xff\xff\xff"
