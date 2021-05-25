30f93de5b81bc038f4ed8afd6d8e4e1a050a13b2
bunderhi <bunderhi@gmail.com>
2021-05-25 10:58:27 -0600

    nextion full test

--- nextionsmltest.py
+++ nextionsmltest.py
@@ -1,4 +1,4 @@
-import time
+import datetime
 from time import sleep
 import subprocess
 import os
@@ -27,6 +27,15 @@ def get_network_interface_state(interface):
     else:
         return status
 
+def startCar():
+    p = None
+    uniq_filename = str(datetime.datetime.now().date()) + '_' + str(datetime.datetime.now().time()).replace(':', '.')
+    f = open(uniq_filename,'wb')
+    p = subprocess.Popen(['python', '-u', '-m', 'manage.py', 'drive'],
+                            stdout=f,
+                            stderr=subprocess.STDOUT)
+    return f,p
+
 # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
 cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
 CPU = subprocess.check_output(cmd, shell = True )
@@ -35,21 +44,66 @@ MemUsage = subprocess.check_output(cmd, shell = True )
 cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
 Disk = subprocess.check_output(cmd, shell = True )
 
-
-netstat = get_ip_address('wlan0')
-if netstat is not None:
-    netstattxt = netstat
-else:
-    netstattxt = 'Down'
-
-freememtxt = str(MemUsage.decode('utf-8'))
-freedisktxt = str(Disk.decode('utf-8'))
-
 ######### make connection to serial UART to read/write NEXTION
 ser = nxlib.ser
 EndCom = "\xff\xff\xff"
 # nxlib.nx_setsys(ser, 'bauds', nxlib.BAUD)  # set default baud (default baud rate of nextion from fabric is 9600)
-
+state = 'Ready'
 nxlib.nx_setsys(ser, 'bkcmd',0)            # sets in NEXTION 'no return error/success codes'
-nxlib.nx_setText(ser, 0,1,'Ready')
-nxlib.nx_setText(ser, 0,3,netstattxt)
+nxlib.nx_setText(ser, 0,1,state)
+nxlib.nx_setText(ser, 1,1,state)
+nxlib.nx_setText(ser, 0,3,'Start')
+nxlib.nx_setText(ser, 1,3,'Stop')
+
+look_touch = 1  # in seconds
+while True:
+    try:
+        touch=ser.read_until(EndCom)
+        if  hex(touch[0]) == '0x65':  #  touch event. If it's empty, do nothing
+            pageID_touch = touch[1]
+            compID_touch = touch[2]
+            event_touch = touch[3]
+            print("page= {}, component= {}, event= {}".format(pageID_touch,compID_touch,event_touch))
+
+            if (pageID_touch, compID_touch) == (0, 3):  # Start Button pressed
+                f,p = startCar()
+                state = 'Running'
+                nxlib.nx_setText(ser, 0,1,state)
+                nxlib.nx_setText(ser, 1,1,state)
+                nxlib.nx_setcmd_1par(ser, 'page', 1)
+             
+            if (pageID_touch, compID_touch) == (1, 3):  # Stop Button pressed
+                p.terminate
+                try:
+                    p.wait(timeout=0.2)
+                    print('== subprocess exited with rc =',p.returncode)
+                    f.close 
+                    state = 'Stopped'
+                    nxlib.nx_setText(ser, 0,1,state)
+                    nxlib.nx_setText(ser, 1,1,state)
+                    nxlib.nx_setcmd_1par(ser, 'page', 0)
+                except subprocess.TimeoutExpired:
+                    print('subprocess did not terminate in time')
+                    nxlib.nx_setText(ser, 0,1,'Car stop failed')
+                    nxlib.nx_setText(ser, 1,1,'Car stop failed')
+                    nxlib.nx_setcmd_1par(ser, 'page', 1)
+        
+        if state == 'running':
+            rc = p.poll()
+            if rc is not None:
+                state = 'Stopped'
+                nxlib.nx_setText(ser, 0,1,'Car stopped rc='+rc)
+                nxlib.nx_setText(ser, 1,1,'Car stopped rc='+rc)
+                nxlib.nx_setcmd_1par(ser, 'page', 0)
+
+        netstat = get_ip_address('wlan0')
+        if netstat is not None:
+            netstattxt = netstat
+        else:
+            netstattxt = 'Network Down'
+        nxlib.nx_setText(ser, 0,2,netstattxt)
+        nxlib.nx_setText(ser, 1,2,netstattxt)
+
+        sleep(look_touch)  ### timeout the bigger the larger the chance of missing a push
+    except:
+        pass
